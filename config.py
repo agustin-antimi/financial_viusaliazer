7682af1cd61eb527e41a9c5bd1766ae71139459d
Antimi Agustin <antimiagustin@gmail.com>
2026-07-06 22:53:25 -0300

    Added config.py with const

---
+++ config.py
@@ -0,0 +1,12 @@
+CHART_COLORS = {
+    "background": "#131722",
+    "text": "#d1d4dc",
+    "grid": "#1e222d",
+    "border": "#2B2B43",
+    "up": "#26a69a",
+    "down": "#ef5350",
+}
+
+PERIODS = ["1mo", "3mo", "6mo", "1y", "2y", "5y", "max"]
+DEFAULT_PERIOD_INDEX = 3
+DEFAULT_TICKER = "AAPL"
